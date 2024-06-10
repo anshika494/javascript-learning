1aa4bb700615c682076cb3f2f4bb947c18a6547a
Anshika Yadav <146864436+anshika494@users.noreply.github.com>
2024-06-10 09:45:34 +0000

    conversion of Datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,7 +12,7 @@ let valueInNumber = Number(score)
 
 isLoggedIn = ""
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true ; 0 => false
 // "" => false
@@ -21,5 +21,24 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *************** Operations ***************
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " world"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
