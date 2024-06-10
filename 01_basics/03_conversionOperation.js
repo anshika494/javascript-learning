f6505270872b510d4dcdcbd05e5be0cd46ac5a47
Anshika Yadav <146864436+anshika494@users.noreply.github.com>
2024-06-10 09:08:33 +0000

    conversion of Datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "33a"
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1 ; false => 0 
+
+isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true ; 0 => false
+// "" => false
+// " " => true
+// "anshika" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
