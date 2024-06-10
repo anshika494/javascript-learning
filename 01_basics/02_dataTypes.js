8849745995a9b38d3dec656af4be640e58f4acdc
Anshika Yadav <146864436+anshika494@users.noreply.github.com>
2024-06-10 09:08:08 +0000

    types of Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") we are using nodejs, not browser
+console.log(3+3);  //code readablilty should be high 
+
+let name = "Anshika"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => " " or ''
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
