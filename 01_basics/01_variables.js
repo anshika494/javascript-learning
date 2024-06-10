962798e8f4b432ddc78500f0dbb5a12d4f990eff
Anshika Yadav <146864436+anshika494@users.noreply.github.com>
2024-06-10 08:18:27 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+ const accountId = 144552
+ let accountEmail = "hello@gmail.com"
+ var accoundPassword = "23g12"
+ accountCity = "Jaipur"
+
+ /*
+Prefer not to use var
+because of issue in block scope and functional scope
+ */
+ accountEmail = "hihi@gmail.com"
+ accountPassword = "876ty"
+ accountCity = "Delhi"
+
+ console.log(accountId)
+ console.log(accountEmail)
+ console.log(accountPassword)
+ console.log(accountCity)
